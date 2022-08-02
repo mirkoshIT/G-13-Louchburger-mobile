c1b40738a7c027a14ca5eaaa464ee63556e43c9b
mirkoshIT <boomazik724@gmail.com>
2022-08-02 19:03:38 +0500

    login in page bitti

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,7 @@ export function Main(){
             {/* <Mirkomil/> */}
             {/* <Muhammadsaid/> */}
             {/* <Bobur/> */}
-            <SignUp/>
+            {/* <SignUp/> */}
             <SignIn/>
 
         </div>
